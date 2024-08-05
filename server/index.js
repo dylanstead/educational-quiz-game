09b7a5bc6b874717cff0bda44666227188a54c8f
Radiah <radiaaahhh@gmail.com>
2024-08-05 15:35:57 +0100

    Fix error in index.js

--- server/index.js
+++ server/index.js
@@ -3,8 +3,6 @@ const app = require("./app");
 
 const port = process.env.PORT;
 
-app.listen(
-  (port, () => {
-    console.log(`Server listening on port ${port}`);
-  })
-);
+app.listen(port, () => {
+  console.log(`Server listening on ${port}`);
+});

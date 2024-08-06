8ecc2d8df38643be230049359e13c535f1003569
dylanstead <dylanstead@gmail.com>
2024-08-06 12:56:21 +0100

    Add gamepage scripts file

--- client/register.js
+++ client/register.js
@@ -1,32 +1,34 @@
-document.getElementById('registerForm').addEventListener('submit', async (event) => {
+document
+  .getElementById("registerForm")
+  .addEventListener("submit", async (event) => {
     event.preventDefault();
-    
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
 
     const userData = {
-        username: username,
-        password: password
+      username: username,
+      password: password,
     };
 
     try {
-        const response = await fetch('http://localhost:3000/users/register', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(userData),
-        });
+      const response = await fetch("http://localhost:3000/users/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userData),
+      });
 
-        if (response.ok) {
-            alert('Registration successful!');
-            window.location.href = 'login.html';
-        } else {
-            const errorData = await response.json();
-            alert(`Error: ${errorData.error}`);
-        }
+      if (response.ok) {
+        alert("Registration successful!");
+        window.location.href = "login.html";
+      } else {
+        const errorData = await response.json();
+        alert(`Error: ${errorData.error}`);
+      }
     } catch (error) {
-        console.error('Error:', error);
-        alert('An error occurred during registration.');
+      console.error("Error:", error);
+      alert("An error occurred during registration.");
     }
-});
+  });

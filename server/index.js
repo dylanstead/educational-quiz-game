7e84e488fb78c04572fab1098f43b20476065428
dylanstead <dylanstead@gmail.com>
2024-08-07 14:56:49 +0100

    final go

--- server/index.js
+++ server/index.js
@@ -1,11 +1,31 @@
+// require("dotenv").config();
+// const app = require("./app");
+
+// const port = process.env.PORT || 3000;
+
+// app.get("/config", (req, res) => {
+//   res.json({
+//     apiUrl: process.env.BACKEND_URL || `http://${req.hostname}:${port}`,
+//   });
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server listening on ${port}`);
+// });
+
 require("dotenv").config();
 const app = require("./app");
 
-const port = process.env.PORT || 3000;
+const isProduction = process.env.NODE_ENV === "production";
+
+const port = isProduction ? process.env.PROD_PORT : process.env.PORT;
+const backendUrl = isProduction
+  ? process.env.PROD_BACKEND_URL
+  : process.env.BACKEND_URL;
 
 app.get("/config", (req, res) => {
   res.json({
-    apiUrl: process.env.BACKEND_URL || `http://${req.hostname}:${port}`,
+    apiUrl: backendUrl || `http://${req.hostname}:${port}`,
   });
 });
 
